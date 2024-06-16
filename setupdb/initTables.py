89f28276fd9c95c9d825e7650655d52cff598bbb
sisixili <sisixili@gmail.com>
2024-06-16 17:07:36 -0400

    Changed table name to Players

--- setupdb/initTables.py
+++ setupdb/initTables.py
@@ -30,7 +30,7 @@ cursor.execute("USE `baseball-dev`;")
 
 # Create table People
 create_table_query = """
-CREATE TABLE IF NOT EXISTS People (
+CREATE TABLE IF NOT EXISTS Players (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     playerID VARCHAR(255),
     birthYear INT,
@@ -72,7 +72,7 @@ def nan_to_none(row):
 data_tuples = people_df.apply(lambda row: nan_to_none(tuple(row)), axis=1)
 
 insert_query = """
-    INSERT INTO People (
+    INSERT INTO Players (
         playerID, birthYear, birthMonth, birthDay, birthCity, birthCountry, birthState, deathYear, deathMonth, deathDay, deathCountry, deathState, deathCity, nameFirst, nameLast, nameGiven, weight, height, bats, throws, debut, bbrefID, finalGame, retroID
     ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
@@ -83,7 +83,7 @@ for data in data_tuples:
 
 conn.commit()
 
-print("Successfully populated People table")
+print("Successfully populated Players table")
 
 # Close the connection
 cursor.close()
