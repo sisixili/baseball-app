84058f9e43191881c5d892ef64855804bb4c15d5
sisixili <sisixili@gmail.com>
2024-06-17 01:55:14 -0400

    added Search All Players, Leaderboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Home from './pages/Home.js'
-import Players from './pages/Players.js'
+import Player from './pages/Player.js'
+import Players from './pages/AllPlayers.js'
+import Leaderboard from './pages/Leaderboard.js'
 import Login from './pages/Login.js'
 import Register from './pages/Register.js'
 
@@ -11,13 +13,14 @@ function App() {
       <Router>
         <nav className = "navBar">
         <Link to="/"> Home </Link> | 
-        <Link to="/player"> All Players List | </Link> 
-        <Link to="/login"> Login | </Link> 
-        <Link to="/register"> Register </Link> 
+        <Link to="/allplayers"> Search All Players | </Link> 
+        <Link to="/leaderboard"> Leaderboard </Link> | 
         </nav>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/player' element={<Players/>} />
+          <Route path="/allplayers/:playerID" element={<Player />} />
+          <Route path='/allplayers' element={<Players/>} />
+          <Route path='/leaderboard' element={<Leaderboard/>} />
           <Route path='/login' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
         </Routes>
---
+++ client/src/pages/Player.js
@@ -0,0 +1,46 @@
+// Single Player component 
+// To be in Leaderboard, Players
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Player() {
+    const { playerID } = useParams();
+    const [PlayerStats, setPlayerStats] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/allplayers/${playerID}`, {})
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            } else {
+              console.log("FETCH failed");
+            }
+          })
+          .then((data) => {
+            if (data.error) {
+              console.log(data.error);
+            } else {
+              setPlayerStats(data);
+              console.log("FETCHED: ", data, " from ", playerID)
+            }
+          })
+          .catch((error) => console.log("ERROR", error));
+      }, [playerID]);
+
+
+    return (
+        <div>
+            {PlayerStats.map((player, key) => (
+          <div key={player.playerID}> {/* TODO: unique key <div key={`${player.playerID}-${player.year}-${player.stint}`}>*/}
+            <h2>{player.nameFirst} {player.nameLast}</h2> 
+            <p>Birthdate: {`${player.birthMonth}-${player.birthDay}-${player.birthYear}`}</p>
+            <p>Other stuff about these dudes here</p>
+          </div>
+        ))}
+
+        </div>
+    )
+}
+
+export default Player;
