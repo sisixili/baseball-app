1b67c11f3e3eb2b2d188b0c18b6dab936c090b4c
sisixili <sisixili@gmail.com>
2024-06-20 03:18:33 -0400

    Finished Login/Logout auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,61 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import Home from './pages/Home.js'
 import Player from './pages/Player.js'
 import Players from './pages/AllPlayers.js'
 import Leaderboard from './pages/Leaderboard.js'
 import Login from './pages/Login.js'
 import Register from './pages/Register.js'
+import Logout from './pages/Logout.js'
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Check if user is logged in
+  useEffect(() => {
+    const checkLoggedInStatus = () => {
+      const token = sessionStorage.getItem('accessToken');
+      if (token) {
+        setIsLoggedIn(true);
+        console.log("User is logged in, token found.");
+      } else {
+        setIsLoggedIn(false);
+        console.log("User is not logged in, no token found.");
+      }
+    };
+
+    // Check login status initially
+    checkLoggedInStatus();
+
+    // Listen to storage events to update login status
+    window.addEventListener('storage', checkLoggedInStatus);
+
+    return () => {
+      window.removeEventListener('storage', checkLoggedInStatus);
+    };
+    
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <nav className = "navBar">
-        <Link to="/"> Home </Link> | 
-        <Link to="/allplayers"> Search All Players | </Link> 
-        <Link to="/leaderboard"> Leaderboard </Link> | 
+        <Link to="/home"> | Home |</Link> 
+        <Link to="/allplayers"> Search All Players |</Link> 
+        <Link to="/leaderboard"> Leaderboard |</Link> 
+        {isLoggedIn ? (
+          <Link to="/logout"> Logout |</Link>
+        ):(' ')}
         </nav>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/home' element={<Home/>} />
           <Route path="/allplayers/:playerID" element={<Player />} />
           <Route path='/allplayers' element={<Players/>} />
           <Route path='/leaderboard' element={<Leaderboard/>} />
-          <Route path='/login' element={<Login/>} />
+          <Route path='/' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
+          <Route path='/logout' element={<Logout/>} />
         </Routes>
       </Router>
     </div>
