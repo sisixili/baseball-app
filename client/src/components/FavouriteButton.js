97208ee4c37f782b0ebb4c1869e10af40944a106
sisixili <sisixili@gmail.com>
2024-07-08 03:48:28 -0400

    Added favourite button

---
+++ client/src/components/FavouriteButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const FavouriteButton = ({ userID, type, id }) => {
+  const handleAddFavourite = async () => {
+
+    try {
+      const response = await fetch('http://localhost:3001/favourite', {
+        method: 'POST',
+        headers: {
+          accessToken: sessionStorage.getItem("accessToken"),
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userID, type, id }),
+      });
+
+      if (response.ok) {
+        alert(`${type.charAt(0).toUpperCase() + type.slice(1)} added successfully!`);
+        console.log(await response.json())
+      } else {
+        const errorData = await response.json();
+        alert(`Failed to add ${type}: ${errorData.error}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert(`Failed to add ${type} due to an error.`);
+    }
+  };
+
+  return (
+    <button onClick={handleAddFavourite}>
+      Add Favourite {type.charAt(0).toUpperCase() + type.slice(1)}
+    </button>
+  );
+};
+
+export default FavouriteButton;
