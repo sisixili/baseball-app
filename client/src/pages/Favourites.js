205412a71f17b97ca292b020be5d78555f41f267
celia3evans <ce3evans@uwaterloo.ca>
2024-07-08 20:27:17 -0400

    Favourites centered, Formatted

--- client/src/pages/Favourites.js
+++ client/src/pages/Favourites.js
@@ -38,28 +38,36 @@ function Favourites() {
         <h2>Your Favourites</h2>
       </div>
       <div className="subTitle">
-        <h3>Favourite Franchises</h3>
+        <div className="favHeader"><h3 className="boxMargins">Favourite Franchises</h3></div>
         {listOfFavourites.favouriteFranchises.map((franchise, key) => (
-          <div key={key}>
+          <div className="favList" key={key}>
+            <div className="boxMargins">
             <Link to={`/franchises/${franchise.franchiseID}`}>
               {franchise.franchiseName}
             </Link>
+            </div>
           </div>
         ))}
-        <h3>Favourite Teams</h3>
+        <h3> </h3>
+        <div className="favHeader"><h3 className="boxMargins">Favourite Teams</h3></div>
         {listOfFavourites.favouriteTeams.map((team, key) => (
-          <div key={key}>
+          <div className="favList" key={key}>
+            <div className="boxMargins">
             <Link to={`/teams/${team.teamID}/${team.yearID}`}>
               {team.name} {team.yearID}
             </Link>
+            </div>
           </div>
         ))}
-        <h3>Favourite Players</h3>
+        <h3> </h3>
+        <div className="favHeader"><h3 className="boxMargins">Favourite Players</h3></div>
         {listOfFavourites.favouritePlayers.map((player, key) => (
-          <div key={key}>
+          <div className="favList" key={key}>
+            <div className="boxMargins">
             <Link to={`/players/${player.playerID}`}>
               {player.nameFirst} {player.nameLast}
             </Link>
+            </div>
           </div>
         ))}
       </div>
