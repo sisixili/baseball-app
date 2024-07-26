7c6d9adc5b7414e1b2cf48127ed02d624f54ecc7
celia3evans <ce3evans@uwaterloo.ca>
2024-07-25 21:09:22 -0400

    New Dark Mode - Favourites

--- client/src/pages/Favourites.js
+++ client/src/pages/Favourites.js
@@ -38,37 +38,46 @@ function Favourites({lightMode, setLightMode}) {
       <div className="subTitle">
         <h3> </h3>
         <div className={lightMode ? "favHeader" : "dmfavHeader"}><h3 className="boxMargins">Favourite Franchises</h3></div>
-        {listOfFavourites.favouriteFranchises.map((franchise, key) => (
-          <div className="favList" key={key}>
-            <div className="boxMargins">
-            <Link to={`/franchises/${franchise.franchiseID}`}>
-              {franchise.franchiseName}
-            </Link>
+        <div className={lightMode?"favlistgap":"DMfavlistgap"}>
+          {listOfFavourites.favouriteFranchises.map((franchise, key) => (
+            <div className={lightMode?"favList":"DMfavList"} key={key}>
+              <div className="boxMargins">
+              <Link className={lightMode?"tableLinks":"DMtableLinks"} to={`/franchises/${franchise.franchiseID}`}>
+                {franchise.franchiseName}
+              </Link>
+              </div>
             </div>
-          </div>
-        ))}
-        <h3> </h3>
+          ))}
+          <p> </p>
+        </div>
+        <p> </p>
         <div className={lightMode ? "favHeader" : "dmfavHeader"}><h3 className="boxMargins">Favourite Teams</h3></div>
-        {listOfFavourites.favouriteTeams.map((team, key) => (
-          <div className="favList" key={key}>
-            <div className="boxMargins">
-            <Link to={`/teams/${team.teamID}/${team.yearID}`}>
-              {team.name} {team.yearID}
-            </Link>
+        <div className={lightMode?"favlistgap":"DMfavlistgap"}>
+          {listOfFavourites.favouriteTeams.map((team, key) => (
+            <div className={lightMode?"favList":"DMfavList"} key={key}>
+              <div className="boxMargins">
+              <Link className={lightMode?"tableLinks":"DMtableLinks"} to={`/teams/${team.teamID}/${team.yearID}`}>
+                {team.name} {team.yearID}
+              </Link>
+              </div>
             </div>
-          </div>
-        ))}
-        <h3> </h3>
+          ))}
+          <p> </p>
+        </div>
+        <p> </p>
         <div className={lightMode ? "favHeader" : "dmfavHeader"}><h3 className="boxMargins">Favourite Players</h3></div>
-        {listOfFavourites.favouritePlayers.map((player, key) => (
-          <div className="favList" key={key}>
-            <div className="boxMargins">
-            <Link to={`/players/${player.playerID}`}>
-              {player.nameFirst} {player.nameLast}
-            </Link>
+        <div className={lightMode?"favlistgap":"DMfavlistgap"}>
+          {listOfFavourites.favouritePlayers.map((player, key) => (
+            <div className={lightMode?"favList":"DMfavList"} key={key}>
+              <div className="boxMargins">
+              <Link className={lightMode?"tableLinks":"DMtableLinks"} to={`/players/${player.playerID}`}>
+                {player.nameFirst} {player.nameLast}
+              </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+          <p> </p>
+        </div>
       </div>
       <h3 className="fav-footer"> </h3>
     </div>
